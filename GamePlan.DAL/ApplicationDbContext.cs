5d91ca9c09c26653b803217147a0607a80852f20
Evgeny Korepanov <te3s.meqa@gmail.com>
2025-01-28 19:47:16 +0300

    Создание репозитория

---
+++ GamePlan.DAL/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GamePlan.DAL
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+	}
+}
---
+++ GamePlan.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using GamePlan.Domain.Interfaces.Repositories;
+
+namespace GamePlan.DAL.Repositories
+{
+	public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public BaseRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public IQueryable<TEntity> GetAll()
+		{
+			return _dbContext.Set<TEntity>();
+		}
+
+		public async Task<TEntity> CreateAsync(TEntity entity)
+		{
+			ArgumentNullException.ThrowIfNull(entity);
+
+			await _dbContext.AddAsync(entity);
+
+			return entity;
+		}
+
+		public TEntity Update(TEntity entity)
+		{
+			ArgumentNullException.ThrowIfNull(entity);
+
+			_dbContext.Update(entity);
+
+			return entity;
+		}
+
+		public void Delete(TEntity entity)
+		{
+			ArgumentNullException.ThrowIfNull(entity);
+
+			_dbContext.Remove(entity);
+		}
+
+		public async Task<int> SaveChangeAsync(TEntity entity)
+		{
+			return await _dbContext.SaveChangesAsync();
+		}
+	}
+}
